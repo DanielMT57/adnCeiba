bd392b5fb939c729fec5acb27fb17ab113e1b1e1
daniel.moncada <daniel.moncada@DANIEL-MONCADA.ceiba.local>
2019-05-02 11:57:11 -0500

    Se modifican los archivos base del proyecto

---
+++ src/main/java/co/com/ceiba/parqueadero/config/ParqueaderoApp.java
@@ -0,0 +1,12 @@
+package co.com.ceiba.parqueadero.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParqueaderoApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParqueaderoApp.class, args);
+	}
+}
